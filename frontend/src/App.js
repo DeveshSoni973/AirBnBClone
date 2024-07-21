f6f5eab1bccf8c82b443f408cdacd1aecf05101a
testcase-108 <krishnaforwork18@gmail.com>
2024-07-21 09:15:37 +0530

    FrontEnd start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Home from './containers/Home'
+import About from './containers/About'
+import Contact from './containers/Contact'
+import ListingDetail from './containers/ListingDetails'
+import Listings from './containers/Listings'
+import Login from './containers/Login'
+import signUp from './containers/SignUp'
+import layout from './hocs/Layout'
+import NotFound from './components/NotFound'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  <Router>
+    <layout>
+      <Switch>
+        <Route exact path = '/' component={Home} />
+        <Route exact path = '/about' component={About} />
+        <Route exact path = '/contact' component={Contact} />
+        <Route exact path = '/listings' component={Listings} />
+        <Route exact path = '/listings/:id' component={ListingDetail} />
+        <Route exact path = '/login' component={Login} />
+        <Route exact path = '/signup' component={signUp} />
+
+        <Route component={NotFound} />
+      </Switch>
+    </layout>
+  </Router>
+};
 
 export default App;
